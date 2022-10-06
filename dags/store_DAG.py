6b073d22b5a09b328997b89db936d434b499ec78
Gagan Nayyar <gagan.nayyar@outlook.com>
2022-10-06 15:50:08 +0000

    Added new dag file

---
+++ dags/store_DAG.py
@@ -0,0 +1,24 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+from datacleaner import data_cleaner
+
+default_args = {
+    'owner': 'Airflow',
+    'start_date': datetime(2019, 12, 9),
+    'retries': 1,
+    'retry_delay': timedelta(seconds=5)
+}
+
+
+dag=DAG('store_dag',default_args=default_args,schedule_interval='@daily',catchup=False)
+
+t1=BashOperator(task_id='check_file_exists', bash_command='shasum ~/store_files_airflow/raw_store_transactions.csv', 
+                retries=2, retry_delay=timedelta(seconds=15),dag=dag)
+
+t2 = PythonOperator(task_id='clean_csv',
+                python_callable=data_cleaner,
+                dag=dag)
+
+t1 >> t2
